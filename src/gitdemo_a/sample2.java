f1cf6b8dacc8241f5f250559a4eb931ef1eb6a17
sundararao2204 <sundararao2204@gmail.com>
2025-03-24 12:13:49 +0530

    this is gitdemo-a project

---
+++ src/gitdemo_a/sample2.java
@@ -0,0 +1,10 @@
+package gitdemo_a;
+
+public class sample2 {
+
+	public static void main(String[] args) {
+		System.out.println("this is sample2");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+
+public class sample1 {
+
+	public static void main(String[] args) {
+		System.out.println("this is sample project");
+		
+System.out.println("this is sample file1");
+	}
+
+}
